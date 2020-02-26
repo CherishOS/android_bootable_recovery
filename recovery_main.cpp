2542a1490442897588bcfefb9bcb6f34fcced5e4
Alessandro Astone <ales.astone@gmail.com>
2020-02-26 17:25:54 +0100

    recovery: allow opting-in to fastbootd
    Change-Id: Id8228c0782f4520779d3a2116ea11015b64c13c3

--- recovery_main.cpp
+++ recovery_main.cpp
@@ -408,7 +408,8 @@ int main(int argc, char** argv) {
         } else if (option == "reason") {
           reason = optarg;
         } else if (option == "fastboot" &&
-                   android::base::GetBoolProperty("ro.boot.dynamic_partitions", false)) {
+                   (android::base::GetBoolProperty("ro.boot.dynamic_partitions", false) ||
+                    android::base::GetBoolProperty("ro.fastbootd.available", false))) {
           fastboot = true;
         }
         break;
@@ -472,7 +473,8 @@ int main(int argc, char** argv) {
     device->RemoveMenuItemForAction(Device::WIPE_CACHE);
   }
 
-  if (!android::base::GetBoolProperty("ro.boot.dynamic_partitions", false)) {
+  if (!android::base::GetBoolProperty("ro.boot.dynamic_partitions", false) &&
+      !android::base::GetBoolProperty("ro.fastbootd.available", false)) {
     device->RemoveMenuItemForAction(Device::ENTER_FASTBOOT);
   }
 
