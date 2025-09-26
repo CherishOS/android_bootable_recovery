30126cc7c55e75baa4e6ac9b324573c6b85e3677
LuK1337 <priv.luk@gmail.com>
2025-09-26 10:10:33 +0200

    recovery: Make OTA safety check A/B package only
    Change-Id: I4b3824c7ad659c65cded4243d36aad151a53c8af
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- recovery.cpp
+++ recovery.cpp
@@ -211,7 +211,7 @@ static bool ask_to_wipe_data(Device* device) {
   return (chosen_item == 1);
 }
 
-static bool ask_to_cancel_ota(Device* device) {
+bool ask_to_cancel_ota(Device* device) {
   // clang-format off
   std::vector<std::string> headers{
     "Overwrite in-progress update?",
@@ -595,13 +595,6 @@ change_menu:
       case Device::APPLY_UPDATE:
       case Device::ENTER_RESCUE: {
         save_current_log = true;
-
-        if (!IsCancelUpdateSafe(device)) {
-          if (!ask_to_cancel_ota(device)) {
-            break;
-          }
-        }
-
         update_in_progress = true;
         WriteUpdateInProgress();
 
